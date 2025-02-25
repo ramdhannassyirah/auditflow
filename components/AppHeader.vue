0920d3e1254ec4e93dfea73c3684f9ae59c5313b
ramdhannassyirah <ramdhannassyirahh.com>
2025-02-25 13:01:12 +0700

    up

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,22 +1,30 @@
 <template>
   <header>
     <nav class="border-b-2 fixed w-full z-50 bg-white">
-      <div class="max-w-7xl mx-auto flex gap-6 items-center py-5">
+      <div class="max-w-7xl mx-auto flex gap-6 items-center py-3">
         <div class="logo">
           <a href="/" class="font-bold text-2xl"
-            ><span class="text-primary">Audit</span>
-            <span class="text-blue-600">Flow</span>
+            ><span class="text-primary">audit</span>
+            <span class="text-blue-600">flow</span>
           </a>
         </div>
-        <div class="flex-1">
-          <ul class="flex items-center gap-4 font-semibold">
-            <li>Produk</li>
-            <li>Harga</li>
-            <li>Artikel</li>
-            <li>Tentang Kami</li>
+        <div class="flex-1 pl-4">
+          <ul class="flex items-center gap-4 text-sm">
+            <li>
+              <NuxtLink to="/"> Produk </NuxtLink>
+            </li>
+            <li>
+              <NuxtLink to="/"> Harga </NuxtLink>
+            </li>
+            <li>
+              <NuxtLink to="/"> Artikel </NuxtLink>
+            </li>
+            <li>
+              <NuxtLink to="/contact"> Kontak </NuxtLink>
+            </li>
           </ul>
         </div>
-        <div class="flex gap-4">
+        <div class="flex gap-4 text-sm">
           <button
             class="bg-primary px-4 py-2 rounded-md text-white font-semibold"
           >
